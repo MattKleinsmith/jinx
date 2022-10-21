9f0af1592bdbb2f99c98b736a553b4ae8a3d6554
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2022-10-21 17:06:28 -0500

    Add Sara's song by Christobear

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 button.onclick = e => {
-    wrapper.innerHTML = "<img src=dear-sara.png>"
+    wrapper.innerHTML = "<img src=dear-sara.png>";
+    play();
+}
+
+function play() {
+    const audio = new Audio();
+    const source = document.createElement("source");
+    source.src = "Saras_Song.mp3";
+    audio.appendChild(source);
+    audio.play();
 }
